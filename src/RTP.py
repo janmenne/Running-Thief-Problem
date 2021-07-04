814b45e0dd8c747691a51612255c35a6bb461226
Jan <jancodet@gmail.com>
2021-07-04 13:47:04 +0200

    Updated RTP.py

--- src/RTP.py
+++ src/RTP.py
@@ -1,7 +1,7 @@
 from src.Graph import Graph, shortest_path
-from FileManager import FileManager
-from Node import Node
-import MethodCollection as MC
+import MethodCollection as Mc
+import os.path
+from os import path
 
 
 def best_path(filename):
@@ -13,57 +13,85 @@ def best_path(filename):
     graph = Graph()
     graph.import_data(filename)
 
+    visited = []
     last_element = list(graph.allNodes)[-1].name
     route = []
-    getRoute(graph, route, last_element)
+    node = graph.allNodes.pop(0)
+    getRoute(graph, node, route, last_element, visited)
 
-    route.sort(key=take_second, reverse=True)
+    route.sort(key=sortV_W, reverse=True)
     getPerfectRoute(route, graph.max_capacity)
 
 
 def getPerfectRoute(route, max_capacity):
     weight = 0
+    value = 0
     for r in route:
-        if weight + r[2] <= max_capacity:
-            weight += r[2]
-            r[3] = True
-    route.insert(0, ["Knoten", "WDG", "Wert/Gewicht", "Mitnehmen?"])
+        if weight + r[4] <= max_capacity:
+            weight += r[4]
+            r[5] = True
+            value += r[3]
+    route.sort(key=sortNode)
+    route.insert(0, ["Knoten", "Nachbar", "WDG", "Wert", "Gewicht", "Mitnehmen?"])
+    route.append("Mitgenommenes Gewicht: " + str(weight))
+    route.append("Maximales Gewicht: " + str(max_capacity))
+    route.append("Wert der Beute: " + str(value))
     print(*route, sep='\n')
 
 
-def take_second(element):
-    return element[2]
+def sortV_W(element):
+    return element[3] / element[4]
 
 
-def getRoute(graph, route, last_element):
-    if not graph.allNodes:
+def sortNode(element):
+    return element[0]
+
+
+def getRoute(graph, node, route, last_element, visited):
+    if node.name == last_element:
+        route.append([node.name, node.name, 0.000, node.getValue(), node.getWeight(), False])
         return
 
     best_wdg = 0
     best_neighbour = None
 
-    node = graph.allNodes.pop(0)
-
-    if node == last_element:
-        return
-
     for neighbour in node.neighbours:
+        if neighbour in visited:
+            continue
         if neighbour == last_element:
-            break
+            distance = node.getDistanceTo(node.neighbours[neighbour])
+        else:
+            distance = node.getDistanceTo(node.neighbours[neighbour]) + \
+                       shortest_path(graph, node.neighbours[neighbour].name, last_element)[0]
         value = node.neighbours[neighbour].getValue()
-        distance = node.getDistanceTo(node.neighbours[neighbour]) + \
-                   shortest_path(graph, node.neighbours[neighbour].name, last_element)[0]
         weight = node.neighbours[neighbour].getWeight()
-        neighbour_wdg = MC.calculateWDG(value, distance, weight)
+        neighbour_wdg = Mc.calculateWDG(value, distance, weight)
         if best_wdg < neighbour_wdg:
             best_wdg = neighbour_wdg
             best_neighbour = node.neighbours[neighbour]
 
-    route.append([best_neighbour.name, best_wdg, best_neighbour.getValue() / best_neighbour.getWeight(), False])
-
-    # Nachbarn übergeben!
-    getRoute(graph, route, last_element)
+    route.append([node.name, best_neighbour.name, round(best_wdg, 3), node.getValue(), node.getWeight(), False])
+    visited.append(node.name)
+    getRoute(graph, best_neighbour, route, last_element, visited)
 
 
 if __name__ == '__main__':
-    best_path("data/rtp_1_10.txt")
+    print("######### OR-Projekt 1: SS21 WWU Muenster #########")
+    print("Autoren: Jan Menne, Julian Luetke-Lengerich, Nick Salfeld")
+    print("Github: https://github.com/janmenne/Running-Thief-Problem")
+    print("######################################################\n\n")
+
+    files = os.listdir('data/')
+
+    print("###Dateien die zur verfügung stehen:\n")
+    for f in files:
+        print(f)
+    print("\n")
+    p = input("Welche Datei aus /data/.. soll benutzt werden?\n")
+    while not path.exists("data/" + p):
+        print("Die angegebene Datei konnte nicht gefunden werden.")
+        p = input("Welche Datei aus /data/.. soll benutzt werden?\n")
+    print("\n\n")
+    best_path("data/" + p)
+    print("\n\n")
+    input("Press key to end programm...")
